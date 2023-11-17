037b3e6ed51c8abd8816d010c880243ede176839
unknown <sagormdtohid@gmail.com>
2023-11-17 08:43:48 +0600

    'headerComponent'

--- src/App.js
+++ src/App.js
@@ -4,132 +4,46 @@ import img1 from './image/tesla-model-s-p100d-sunrise-mptdvnar4iloeun3.jpg';
 import img2 from './image/tesla-model-s-larte-design-l3lzeq7qeznvycnw.jpg';
 import img3 from './image/598342.jpg';
 import img4 from './image/tesla-cybertruck-concept-cars-3840x2400-882.jpg';
+import img5 from './image/Tesla-Roadster-103.jpg';
+import img6 from './image/0x0-roadster-05-1621869837.jpg';
 import { useEffect, useRef, useState } from 'react';
 import getElementId from './useHooks/getElementId';
+import YouTube from 'react-youtube';
+import Header from './Components/Header/Header';
 
 function App() {
-  // const [selectedElements, setSelectedElements] = useState([]);
-  const [inViewElementId, setInViewElementId] = useState(''); // New state variable
-  // console.log(inViewElementId)
-
-  useEffect(() => {
-    getElementId({ setInViewElementId });
-  }, [])
-
-
-  // navbar 
-  // const [isHovered, setIsHovered] = useState('');
-
-  // const handleMouseEnter = (value) => {
-  //   setIsHovered(value);
-  //   console.log(isHovered)
-  // };
-
-  // const handleMouseLeave = () => {
-  //   setIsHovered(false);
-  // };
-
-
-
-
-  const [isFocused, setIsFocused] = useState(false);
-  const [focusedIn, setFocusedIn] = useState('');
-
-  const handleFocus = () => {
-    setIsFocused(true);
-  };
-
-  const handleBlur = () => {
-    setIsFocused(false);
-  };
-
-  const handleMouseLeave = () => {
-    setIsFocused(false);
-    setFocusedIn('');
-  };
-
-  const handleMouseEnter = (value) => {
-    setIsFocused(true);
-    setFocusedIn(value);
-    console.log(isFocused)
-  };
-
-  console.log(isFocused)
-
-
-
   return (
     <div className="slides">
-      <div onMouseLeave={handleMouseLeave} className={`sticky top-0 ${isFocused && 'bg-white'}  ${inViewElementId === 'bgBlack' ? 'text-white' : 'text-black'}`}>
-        {/* <div className='flex gap-10 mx-auto w-fit'>
-          <button onMouseEnter={() => handleMouseEnter('vehicle')} onMouseLeave={handleMouseLeave} className='p-2 rounded-lg hover:bg-amber-200'>Vehicle</button>
+      <Header></Header>
 
-          <button onMouseEnter={() => handleMouseEnter('energy')} onMouseLeave={handleMouseLeave} className='p-2 rounded-lg hover:bg-amber-200'>Energy</button>
-
-          <button onMouseEnter={() => handleMouseEnter('charging')} onMouseLeave={handleMouseLeave} className='p-2 rounded-lg hover:bg-amber-200'>Charging</button>
-        </div>
-        <div>
-          {
-            isHovered === 'vehicle' &&
-            <div>
-              <h1 className='text-center text-'>vehicle</h1>
-            </div>
-          }
-          {
-            isHovered === 'energy' &&
-            <div>
-              <h1 className='text-center text-'>energy</h1>
-            </div>
-          }
-          {
-            isHovered === 'charging' &&
-            <div>
-              <h1 className='text-center text-'>charging</h1>
-            </div>
-          }
-        </div> */}
-
-
-
-        <div className='flex gap-10 mx-auto w-fit'>
-          <button onMouseEnter={()=>handleMouseEnter('vehicle')} 
-            className={`p-2 rounded-lg hover:bg-amber-200 ${focusedIn ==='vehicle' && 'bg-amber-200'}`}>Vehicle</button>
-
-          <button onMouseEnter={()=>handleMouseEnter('energy')} className={`p-2 rounded-lg hover:bg-amber-200 ${focusedIn ==='energy' && 'bg-amber-200'}`}>Energy</button>
-
-          <button onMouseEnter={()=>handleMouseEnter('charging')} className={`p-2 rounded-lg hover:bg-amber-200 ${focusedIn === 'charging' && 'bg-amber-200'}`}>Charging</button>
-        </div>
-
-        <div>
-          {
-            focusedIn ==='vehicle' && <div>
-              <h1 className='text-center p-5' >vehicle</h1>
-            </div>
-          }
-          {
-            focusedIn ==='energy' && <div>
-              <h1 className='text-center p-5' >energy</h1>
-            </div>
-          }
-          {
-            focusedIn ==='charging' && <div>
-              <h1 className='text-center p-5' >charging</h1>
-            </div>
-          }
-        </div>
+      <div id='noBgBlack' className='slide'>
+        <img src='https://digitalassets.tesla.com/tesla-contents/image/upload/h_1800,w_2880,c_fit,f_auto,q_auto:best/Homepage-Model-X-Desktop-LHD' alt="" />
       </div>
       <div id='noBgBlack' className='slide'>
-        <img src={img1} alt="" />
+        <img src="https://digitalassets.tesla.com/tesla-contents/image/upload/h_1800,w_2880,c_fit,f_auto,q_auto:best/425_HP_SolarPanels_D" alt="" />
       </div>
       <div id='noBgBlack' className='slide'>
-        <img src={img2} alt="" />
+        <img src="https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Powerwall-Desktop.jpg" alt="" />
       </div>
       <div id='noBgBlack' className='slide'>
-        <img src={img3} alt="" />
+        <img src="https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Accessories-Desktop-NA-APAC.jpg" alt="" />
       </div>
       <div id='bgBlack' className='slide'>
         <img src={img4} alt="" />
       </div>
+
+      {/* <div id='noBgBlack' className='slide'>
+        <img src={img6} alt="" />
+      </div>
+      <div id='noBgBlack' className='slide'>
+        <img src="https://digitalassets.tesla.com/tesla-contents/image/upload/h_1800,w_2880,c_fit,f_auto,q_auto:best/Homepage-Model-X-Desktop-LHD" alt="" />
+      </div>
+      <div id='bgBlack' className='slide'>
+        <img src={img4} alt="" />
+      </div>
+      <div id='bgBlack' className='slide'>
+        <img src="https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Model-3-Desktop-NA.jpg" alt="" />
+      </div> */}
     </div>
   );
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,110 @@
+import React, { useEffect, useState } from 'react';
+import getElementId from '../../useHooks/getElementId';
+
+const Header = () => {
+    // const [selectedElements, setSelectedElements] = useState([]);
+    const [inViewElementId, setInViewElementId] = useState(''); // New state variable
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        getElementId({ setInViewElementId });
+    }, [])
+
+
+    // navbar 
+    const [isFocused, setIsFocused] = useState(false);
+    const [focusedIn, setFocusedIn] = useState('');
+
+    const handleMouseLeave = () => {
+        setIsFocused(false);
+        setFocusedIn('');
+    };
+
+    const handleMouseEnter = (value) => {
+        setIsFocused(true);
+        setFocusedIn(value);
+        console.log(isFocused)
+    };
+
+
+    useEffect(() => {
+        fetch('processTeslaApi.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, []);
+
+    return (
+        <div onMouseLeave={handleMouseLeave} className={`sticky top-0 font-serif ${inViewElementId === 'bgBlack' ? 'text-white' : 'text-black'}
+      ${isFocused && 'bg-white !text-black'}`}>
+            <div className='flex gap-10 mx-auto w-fit py-3'>
+                <button onMouseEnter={() => handleMouseEnter('vehicle')}
+                    className={`px-2.5 py-1.5 rounded-lg hover:bg-slate-100	 ${focusedIn === 'vehicle' && 'bg-slate-100'}`}>Vehicles</button>
+
+                <button onMouseEnter={() => handleMouseEnter('energy')} className={`px-2.5 py-1.5 rounded-lg hover:bg-slate-100 ${focusedIn === 'energy' && 'bg-slate-100'}`}>Energy</button>
+
+                <button onMouseEnter={() => handleMouseEnter('charging')} className={`px-2.5 py-1.5 rounded-lg hover:bg-slate-100 ${focusedIn === 'charging' && 'bg-slate-100'}`}>Charging</button>
+
+                <button onMouseEnter={() => handleMouseEnter('shop')} className={`px-2.5 py-1.5 rounded-lg hover:bg-slate-100 ${focusedIn === 'shop' && 'bg-slate-100'}`}>Shop</button>
+            </div>
+            <div>
+                {
+                    focusedIn === 'vehicle' &&
+                    <div className='w-7/12 mx-auto py-10'>
+                        <div className='grid grid-cols-3 gap-20'>
+                            {
+                                products[0].products.map(product =>
+                                    <a href='./' className='text-center'>
+                                        <img src={product.asset} alt="" />
+                                        <h1>{product.title}</h1>
+                                    </a>)
+                            }
+                        </div>
+                    </div>
+                }
+                {
+                    focusedIn === 'energy' && <div className='w-7/12 mx-auto py-10'>
+                        <div className='grid grid-cols-3 gap-20'>
+                            {
+                                products[1].products.map(product =>
+                                    <a href='./' className='text-center'>
+                                        <img src={product.asset} alt="" />
+                                        <h1>{product.title}</h1>
+                                    </a>)
+                            }
+                        </div>
+                    </div>
+                }
+                {
+                    focusedIn === 'charging' &&
+                    <div className='w-7/12 mx-auto py-10'>
+                        <div className='grid grid-cols-3 gap-20'>
+                            {
+                                products[2].products.map(product =>
+                                    <a href='./' className='text-center'>
+                                        <img src={product.asset} alt="" />
+                                        <h1>{product.title}</h1>
+                                    </a>)
+                            }
+                        </div>
+                    </div>
+                }
+                {
+                    focusedIn === 'shop' &&
+                    <div className='w-7/12 mx-auto py-10'>
+                        <div className='grid grid-cols-3 gap-20'>
+                            {
+                                products[3].products.map(product =>
+                                    <a href='./' className='text-center'>
+                                        <img src={product.asset} alt="" />
+                                        <h1>{product.title}</h1>
+                                    </a>)
+                            }
+                        </div>
+                    </div>
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Header;
